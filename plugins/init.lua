ea71aecc8fcec1bfe3dc683a62d5c39d04dbd49e
axbuglak <alexeybuglak24@gmail.com>
2024-04-20 23:17:42 +0200

    updated config

---
+++ plugins/init.lua
@@ -0,0 +1,241 @@
+return {
+  -- You can disable default plugins as follows:
+  { "goolord/alpha-nvim",           enabled = false },
+  { "max397574/better-escape.nvim", enabled = false },
+  -- You can also add new plugins here as well:
+  { "lvimuser/lsp-inlayhints.nvim", config = true },
+  -- using lazy.nvim
+  -- { "AstroNvim/astrotheme", commit = "7a52efdd9a5bd302445d284a424467f92e4b1d44" },
+
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require "astronvim.utils.status"
+      opts.statusline = {
+        -- default highlight for the entire statusline
+        hl = { fg = "fg", bg = "bg" },
+        -- each element following is a component in astronvim.utils.status module
+
+        -- add the vim mode component
+        status.component.mode {
+          -- enable mode text with padding as well as an icon before it
+          mode_text = { icon = { kind = "VimIcon", padding = { right = 1, left = 1 } } },
+          -- surround the component with a separators
+          surround = {
+            -- it's a left element, so use the left separator
+            separator = "left",
+            -- set the color of the surrounding based on the current mode using astronvim.utils.status module
+            color = function() return { main = status.hl.mode_bg(), right = "blank_bg" } end,
+          },
+        },
+        -- we want an empty space here so we can use the component builder to make a new section with just an empty string
+        status.component.builder {
+          { provider = "" },
+          -- define the surrounding separator and colors to be used inside of the component
+          -- and the color to the right of the separated out section
+          surround = { separator = "left", color = { main = "blank_bg", right = "file_info_bg" } },
+        },
+        -- we want an empty space here so we can use the component builder to make a new section with just an empty string
+        -- add a section for the currently opened file information
+        status.component.file_info {
+          -- enable the file_icon and disable the highlighting based on filetype
+          -- file_icon = { padding = { left = 1 } },
+          filename = { fallback = "Empty" },
+          -- add padding
+          padding = { right = 1 },
+          -- define the section separator
+          surround = { separator = "left", condition = false },
+        },
+        -- add a component for the current git branch if it exists and use no separator for the sections
+        status.component.git_branch(),
+        -- add a component for the current git diff if it exists and use no separator for the sections
+        status.component.git_diff { padding = { left = 1 }, surround = { separator = "none" } },
+        -- fill the rest of the statusline
+        -- the elements after this will appear in the middle of the statusline
+        status.component.fill(),
+        status.component.cmd_info(),
+        -- add a component to display if the LSP is loading, disable showing running client names, and use no separator
+        status.component.lsp { lsp_client_names = false, surround = { separator = "none", color = "bg" } },
+        -- fill the rest of the statusline
+        -- the elements after this will appear on the right of the statusline
+        status.component.fill(),
+        -- add a component for the current diagnostics if it exists and use the right separator for the section
+        status.component.diagnostics { surround = { separator = "right" } },
+        -- add a component to display LSP clients, disable showing LSP progress, and use the right separator
+        status.component.lsp { lsp_progress = false, surround = { separator = "right" } },
+        -- NvChad has some nice icons to go along with information, so we can create a parent component to do this
+        -- all of the children of this table will be treated together as a single component
+        {
+          -- define a simple component where the provider is just a folder icon
+          status.component.builder {
+            -- astronvim.get_icon gets the user interface icon for a closed folder with a space after it
+            { provider = require("astronvim.utils").get_icon "FolderClosed" },
+            -- add padding after icon
+            padding = { right = 1 },
+            -- set the foreground color to be used for the icon
+            hl = { fg = "bg" },
+            -- use the right separator and define the background color
+            surround = { separator = "right", color = "folder_icon_bg" },
+          },
+          -- add a file information component and only show the current working directory name
+          status.component.file_info {
+            -- we only want filename to be used and we can change the fname
+            -- function to get the current working directory name
+            filename = { fname = function(nr) return vim.fn.getcwd(nr) end, padding = { left = 1 } },
+            -- disable all other elements of the file_info component
+            file_icon = false,
+            file_modified = false,
+            file_read_only = false,
+            -- use no separator for this part but define a background color
+            surround = { separator = "none", color = "file_info_bg", condition = false },
+          },
+        },
+        -- the final component of the NvChad statusline is the navigation section
+        -- this is very similar to the previous current working directory section with the icon
+        { -- make nav section with icon border
+          -- define a custom component with just a file icon
+          status.component.builder {
+            { provider = require("astronvim.utils").get_icon "ScrollText" },
+            -- add padding after icon
+            padding = { right = 1 },
+            -- set the icon foreground
+            hl = { fg = "bg" },
+            -- use the right separator and define the background color
+            -- as well as the color to the left of the separator
+            surround = { separator = "right", color = { main = "nav_icon_bg", left = "file_info_bg" } },
+          },
+          -- add a navigation component and just display the percentage of progress in the file
+          status.component.nav {
+            -- add some padding for the percentage provider
+            percentage = { padding = { right = 1 } },
+            -- disable all other providers
+            ruler = false,
+            scrollbar = false,
+            -- use no separator and define the background color
+            surround = { separator = "none", color = "file_info_bg" },
+          },
+        },
+      }
+
+      -- return the final options table
+      return opts
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "Mocha",
+      color_overrides = {
+        mocha = {
+          rosewater = "#efc9c2",
+          flamingo = "#ebb2b2",
+          pink = "#f2a7de",
+          mauve = "#b889f4",
+          red = "#ea7183",
+          maroon = "#ea838c",
+          peach = "#f39967",
+          yellow = "#eaca89",
+          green = "#96d382",
+          teal = "#78cec1",
+          sky = "#91d7e3",
+          sapphire = "#68bae0",
+          blue = "#739df2",
+          lavender = "#a0a8f6",
+          text = "#b5c1f1",
+          subtext1 = "#a6b0d8",
+          subtext0 = "#959ec2",
+          overlay2 = "#848cad",
+          overlay1 = "#717997",
+          overlay0 = "#63677f",
+          surface2 = "#505469",
+          surface1 = "#3e4255",
+          surface0 = "#2c2f40",
+          base = "#1a1c2a",
+          mantle = "#141620",
+          crust = "#0e0f16",
+        },
+      },
+      transparent_background = false,
+      term_colors = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      local bg = "#011628"
+      local bg_dark = "#011423"
+      local bg_highlight = "#143652"
+      local bg_search = "#0A64AC"
+      local bg_visual = "#275378"
+      local fg = "#CBE0F0"
+      local fg_dark = "#B4D0E9"
+      local fg_gutter = "#627E97"
+      local border = "#547998"
+
+      require("tokyonight").setup({
+        style = "night",
+        on_colors = function(colors)
+          colors.bg = bg
+          colors.bg_dark = bg_dark
+          colors.bg_float = bg_dark
+          colors.bg_highlight = bg_highlight
+          colors.bg_popup = bg_dark
+          colors.bg_search = bg_search
+          colors.bg_sidebar = bg_dark
+          colors.bg_statusline = bg_dark
+          colors.bg_visual = bg_visual
+          colors.border = border
+          colors.fg = fg
+          colors.fg_dark = fg_dark
+          colors.fg_float = fg
+          colors.fg_gutter = fg_gutter
+          colors.fg_sidebar = fg_dark
+        end
+      })
+    end
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "BufRead",
+    config = function()
+      require("neoscroll").setup {
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+        hide_cursor = true,          -- Hide cursor while scrolling
+        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,       -- Default easing function
+        pre_hook = nil,              -- Function to run before the scrolling animation starts
+        post_hook = nil,             -- Function to run after the scrolling animation ends
+        performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+      }
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+    keys = {
+      { "<leader>xx", "<cmd>TroubleToggle<CR>",                       desc = "Open/close trouble list" },
+      { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
+      { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>",  desc = "Open trouble document diagnostics" },
+      { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>",              desc = "Open trouble quickfix list" },
+      { "<leader>xl", "<cmd>TroubleToggle loclist<CR>",               desc = "Open trouble location list" },
+      { "<leader>xt", "<cmd>TodoTrouble<CR>",                         desc = "Open todos in trouble" },
+    },
+  }
+}
